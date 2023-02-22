e33860754dacd1b9871a988cf4f148e57d9d97ac
Melihcan Şimşek <melihsimsek02@gmail.com>
2023-02-22 18:29:37 +0300

    22/02/2023

---
+++ src/app/models/rental.ts
@@ -0,0 +1,10 @@
+export interface Rental{
+    colorName:string;
+    brandName:string;
+    carName:string;
+    customerName:string;
+    modelYear:string;
+    carDescription:string;
+    rentDate:Date;
+    returnDate:Date;
+}
---
+++ src/app/services/rental.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RentalResponseModel } from '../models/rentalResponseModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RentalService {
+  apiUrl = 'https://localhost:7103/api/Rentals/getrentaldetails';
+  constructor(private httpClient: HttpClient) {}
+
+  getRentals(): Observable<RentalResponseModel> {
+    return this.httpClient.get<RentalResponseModel>(this.apiUrl);
+  }
+}
